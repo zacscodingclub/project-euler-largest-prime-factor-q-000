d79399d1dfa967327974162c0358429d8d3de9a8
zacscodingclub <zbaston@gmail.com>
2016-10-23 16:55:59 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,7 @@
 # Enter your procedural solution here!
+require 'prime'
+
+def largest_prime_factor(value)
+  all_primes = Prime.prime_division(value).flatten.uniq.sort
+  answer = all_primes[-1]
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+  def initialize(value)
+  	@value = value
+  end
+
+  def number
+    all_primes = Prime.prime_division(@value).flatten.uniq.sort
+    all_primes[-1]
+  end
+end
